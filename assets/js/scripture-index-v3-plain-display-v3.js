ead18090f0f878ea1b88a8e85763f24e19a6a2f5
Vyas Muni <319807238+krishnavyasmuni@users.noreply.github.com>
2026-08-26 17:38:08 +0100

    Fix Linga card transliteration

--- assets/js/scripture-index-v3-plain-display-v3.js
+++ assets/js/scripture-index-v3-plain-display-v3.js
@@ -4,7 +4,7 @@
   const roman=s=>String(s??'')
     .replace(/Ś/g,'Sh').replace(/ś/g,'sh').replace(/Ṣ/g,'Sh').replace(/ṣ/g,'sh')
     .replace(/Ṛ/g,'Ri').replace(/ṛ/g,'ri').replace(/Ṝ/g,'Ri').replace(/ṝ/g,'ri')
-    .replace(/Ṅ/g,'Ng').replace(/ṅ/g,'ng').replace(/Ñ/g,'Ny').replace(/ñ/g,'ny')
+    .replace(/Ṅ/g,'N').replace(/ṅ/g,'n').replace(/Ñ/g,'Ny').replace(/ñ/g,'ny')
     .replace(/Ṃ|Ṁ/g,'M').replace(/ṃ|ṁ/g,'m').replace(/Ḥ/g,'H').replace(/ḥ/g,'h')
     .replace(/Ṭ/g,'T').replace(/ṭ/g,'t').replace(/Ḍ/g,'D').replace(/ḍ/g,'d').replace(/Ṇ/g,'N').replace(/ṇ/g,'n')
     .normalize('NFD').replace(/[\u0300-\u036f]/g,'').normalize('NFC');
